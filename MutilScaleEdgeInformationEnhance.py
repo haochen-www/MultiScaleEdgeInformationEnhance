86fd3ab99e2240e2dace360a3a23b0a5ecfcaa06
haochen-www <2608534587@qq.com>
2025-04-21 17:19:34 +0800

    MutilScaleEdgeInformationEnhance

---
+++ MutilScaleEdgeInformationEnhance.py
@@ -0,0 +1,61 @@
+class EdgeEnhancer(nn.Module):
+    def __init__(self, in_dim):
+        super().__init__()
+        self.out_conv = Conv(in_dim, in_dim, act=nn.Sigmoid())
+        self.pool = nn.AvgPool2d(3, stride=1, padding=1)
+        self.pool_h = nn.AdaptiveAvgPool2d((None, 1))
+        self.pool_w = nn.AdaptiveAvgPool2d((1, None))
+        gc = in_dim // 2
+        self.excite = nn.Sequential(
+            nn.Conv2d(in_dim, gc, kernel_size=(1, 3), padding=(0, 3 // 2), groups=gc),
+            nn.BatchNorm2d(gc),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(gc, in_dim, kernel_size=(3, 1), padding=(3 // 2, 0), groups=gc),
+            nn.Sigmoid()
+        )
+    def sge(self, x):
+        # [N, D, C, 1]
+        x_h = self.pool_h(x)
+        x_w = self.pool_w(x)
+        x_gather = x_h + x_w  # .repeat(1,1,1,x_w.shape[-1])
+        ge = self.excite(x_gather)  # [N, 1, C, 1]
+        return ge
+    def forward(self, x):
+        edge = self.pool(x)
+        edge = x - edge
+        edge = self.sge(edge)
+        out = edge + x
+        return out
+
+
+class MutilScaleEdgeInformationEnhance(nn.Module):
+    def __init__(self, inc, bins):
+        super().__init__()
+
+        self.features = []
+        for bin in bins:
+            self.features.append(nn.Sequential(
+                nn.AdaptiveAvgPool2d(bin),
+                Conv(inc, inc // len(bins), 1),
+                Conv(inc // len(bins), inc // len(bins), 3, g=inc // len(bins))
+            ))
+        self.ees = []
+        for _ in bins:
+            self.ees.append(EdgeEnhancer(inc // len(bins)))
+        self.features = nn.ModuleList(self.features)
+        self.ees = nn.ModuleList(self.ees)
+        self.local_conv = Conv(inc, inc, 3)
+        self.final_conv = Conv(inc * 2, inc)
+
+    def forward(self, x):
+        x_size = x.size()
+        out = [self.local_conv(x)]
+        for idx, f in enumerate(self.features):
+            out.append(self.ees[idx](F.interpolate(f(x), x_size[2:], mode='bilinear', align_corners=True)))
+        return self.final_conv(torch.cat(out, 1))
+
+
+class CSP_MutilScaleEdgeInformationEnhance(C2f):
+    def __init__(self, c1, c2, n=1, shortcut=False, g=1, e=0.5):
+        super().__init__(c1, c2, n, shortcut, g, e)
+        self.m = nn.ModuleList(MutilScaleEdgeInformationEnhance(self.c, [3, 6, 9, 12]) for _ in range(n))
